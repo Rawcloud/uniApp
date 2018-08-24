e436e509df2926c010e4f7f690287ea8fd957ef3
Rawcloud <wu20100319@163.com>
2018-08-24 17:51:25 +0800

    Add files via upload
    通用标题头

---
+++ components/page-head.vue
@@ -0,0 +1,25 @@
+<template name="page-head">
+	<view class="page-head">
+		<view class="uni-icon uni-icon-arrowleft" style="float: left; margin-top: 15px;" @click="toback()"></view>
+		<view class="page-head-title">{{title}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "page-head",
+		props: {
+			title: {
+				type: String,
+				default: ""
+			}
+		},
+		methods:{
+			toback:function(){
+				uni.navigateBack({
+					delta:1
+				})
+			}
+		}
+	}
+</script>
